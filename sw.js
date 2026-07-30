9214920552ef6ccbba781664aa87be7159025632
jeffshama <jeffshama@hotmail.com>
2026-07-29 23:51:32 -0400

    Final Release

--- sw.js
+++ sw.js
@@ -1,37 +1,38 @@
+const CACHE_VERSION = "20260729234317";
+const CACHE_NAME = "stoner-cache-" + CACHE_VERSION;
 
-const CACHE_VERSION = '20260729180221';
-const CACHE_NAME = 'stoner-cache-' + CACHE_VERSION;
-
-const ASSETS = [
-  'index.html',
-  'style.css',
-  'manifest.json',
-  'images/splash.png',
-  'images/silhouette.png'
+const FILES_TO_CACHE = [
+    "index.html",
+    "style.css",
+    "manifest.json",
+    "images/splash.png",
+    "images/silhouette.png"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(k => {
-          if (k !== CACHE_NAME && k.startsWith('stoner-cache-')) {
-            return caches.delete(k);
-          }
-        })
-      )
-    )
-  );
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            )
+        )
+    );
+    self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(resp => resp || fetch(event.request))
+    );
 });
