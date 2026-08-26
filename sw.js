ce931b70c7726adb513c3fd982ba16ec8552ddfc
jeffshama <65435006+jeffshama@users.noreply.github.com>
2026-08-26 05:24:24 -0400

    New VS Code Compiler
    New compiler

--- sw.js
+++ sw.js
@@ -1,38 +1,35 @@
-const CACHE_VERSION = "20260730000812";
-const CACHE_NAME = "stoner-cache-" + CACHE_VERSION;
+const CACHE_VERSION = '20260826052108';
+const CACHE_NAME = 'stoner-cache-' + CACHE_VERSION;
 
-const FILES_TO_CACHE = [
-    "index.html",
-    "style.css",
-    "manifest.json",
-    "images/splash.png",
-    "images/silhouette.png"
-];
-
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-    );
-    self.skipWaiting();
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        'index.html',
+        'style.css',
+        'manifest.json',
+        'images/splash.png',
+        'images/silhouette.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.map(key => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            )
-        )
-    );
-    self.clients.claim();
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => !k.includes(CACHE_VERSION))
+            .map(k => caches.delete(k))
+      );
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(resp => resp || fetch(event.request))
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
 });
